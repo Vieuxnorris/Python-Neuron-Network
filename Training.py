201a74675668fdb08e40c52a31c0c60f52c687e5
Vieuxnorris <julien.wins@std.heh.be>
2021-02-21 23:38:32 +0100

    Update Training.py

--- Training.py
+++ Training.py
@@ -10,6 +10,18 @@ from nnfs.datasets import spiral_data
 import numpy as np
 import nnfs
 
+class Activation_Softmax:
+    
+    def forward(self, inputs):
+        exp_values = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
+        probabilities = exp_values / np.sum(exp_values, axis=1, keepdims=True)
+        self.output = probabilities
+
+class Activation_ReLU:
+    
+    def forward(self, inputs):
+        self.output = np.maximum(0, inputs)
+
 class Layer_Dense:
     
     def __init__(self, n_inputs, n_neurons):
@@ -22,10 +34,22 @@ class Layer_Dense:
 
 nnfs.init()
 
-X, y = spiral_data(samples=20, classes=3)
+X, y = spiral_data(samples=100, classes=3)
 
 dense1 = Layer_Dense(2, 3)
 
+activation1 = Activation_ReLU()
+
+dense2 = Layer_Dense(3,3)
+
+activation2 = Activation_Softmax()
+
 dense1.forward(X)
 
-print(dense1.output[:5])
+activation1.forward(dense1.output)
+
+dense2.forward(activation1.output)
+
+activation2.forward(dense2.output)
+
+print(activation2.output[:5])
