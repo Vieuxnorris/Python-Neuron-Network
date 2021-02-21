2b506b384e1a6b1c9c256907eddb8245e636991e
Vieuxnorris <vieuxnorris@gmail.com>
2021-02-21 23:36:09 +0100

    Add files via upload

---
+++ ReLU.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 21 19:41:32 2021
+
+@author: vieuxnorris
+"""
+
+import numpy as np
+
+inputs = [0,2,-1,3.3,-2.7,1.1,2.2,-100]
+output = np.maximum(0, inputs)
+        
+print(output)
